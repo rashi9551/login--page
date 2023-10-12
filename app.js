5706637bf392dcb48a897f71bc2bf8e81e004974
muhammed rashid <rashidroz1361@gmail.com>
2023-10-12 00:44:59 -0700

    half

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+// modules import  
 const express=require('express')
+const userrouter=require("./router/router")
 const app=express()
 
-const userrouter=require("./router/router")
+
+// view engine 
+app.set("view engine","hbs")
+app.use(express.urlencoded({extended:true}))
+
+
+// router connect  
 app.use("/",userrouter)
 
+    
+
+// host   
 app.listen(3000)
--- router/router.js
+++ router/router.js
@@ -1,9 +1,17 @@
+const { log } = require('console')
 const express=require('express')
 const router=express.Router()
 router.get("/",(req,res)=>{
-    res.send("im heare")
+    res.render("login")
 })
-
+router.post("/home",(req,res)=>{
+    res.render("home")
+    
+    
+})
+router.get("/logout",(req,res)=>{
+    res.redirect('/')
+})
 
 
 module.exports=router
